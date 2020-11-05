c644ef3e85678bae068df46bfbe2a216f21e1ba7
vbchaure <67856458+vbchaure@users.noreply.github.com>
2020-11-05 10:37:53 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+	 <div id="fixedfooter"> 
+	<span class="floating-call-button"><a href="tel:+918470010001"><img src="/sites/all/themes/freeprojectz/img/Nx50xcall-button.png.pagespeed.ic.YwDo_mDRLc.webp" style="height:50px" data-pagespeed-url-hash="2312200765" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></a></span>
+	<span class="floating-call-button"><a href="https://api.whatsapp.com/send?phone=919552337996&text=Hello"><img src="/sites/all/themes/freeprojectz/img/Nx50xwhatsappn.png.pagespeed.ic.OT4BZaf2DY.webp" style="height:50px"  onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></a></span>
+    </div>
+</body>
+</html>
